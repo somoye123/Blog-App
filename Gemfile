9f406d1b565de5ccdf5d9594aaefbf4653e7359b
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-24 16:10:03 -0700

    fix linter errors

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3', '~> 1.4'
 group :development, :test do
- gem 'sqlite3'
+  gem 'sqlite3'
 end
 
 group :production do
@@ -32,14 +32,14 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
-#A library that manages file attachments and uploading 
-gem "paperclip"
+# A library that manages file attachments and uploading
+gem 'paperclip'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
@@ -56,6 +56,6 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'hirb'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,47 +1,46 @@
 class ArticlesController < ApplicationController
-    include ArticlesHelper
+  include ArticlesHelper
 
-    def index
-        @articles = Article.all
-    end
-    
-    def show
-        @article = Article.find(params[:id])
-        @comment = Comment.new
-        @comment.article_id = @article.id
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def new
-        @article = Article.new
-    end
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
 
-    def create
-        @article = Article.new(article_params)
-        @article.save
+  def new
+    @article = Article.new
+  end
 
-        flash.notice = "Article '#{@article.title}' Created!"
-        redirect_to article_path(@article)
-    end
+  def create
+    @article = Article.new(article_params)
+    @article.save
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+    flash.notice = "Article '#{@article.title}' Created!"
+    redirect_to article_path(@article)
+  end
 
-    def update
-        @article = Article.find(params[:id])
-        @article.update(article_params)
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-        flash.notice = "Article '#{@article.title}' Updated!"
-      
-        redirect_to article_path(@article)
-    end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
 
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
+    flash.notice = "Article '#{@article.title}' Updated!"
 
-        flash.notice = "Article '#{@article.title}' Deleted!"
-        redirect_to articles_path
-    end
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    redirect_to articles_path
+  end
 end
- 
